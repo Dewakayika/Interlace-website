b87316fd00165cf26a47a606383cea4b88db8924
Dewakayika <kayikadewa@gmail.com>
2025-05-21 21:00:29 +0800

    Update: Clean up import statements and enhance HTML structure in layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,14 @@
 import './globals.css'
-import { Inter , Lexend } from 'next/font/google'
+import { Inter, Lexend } from 'next/font/google'
 import ContactPopup from './components/ContactPopup'
-import { Analytics } from '@vercel/analytics/next';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from '@vercel/analytics/next'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 const inter = Inter({ 
   subsets: ['latin'],
   weight: ['200','300','400','500','600', '700','800'],
   variable: '--font-inter',
-});
+})
 
 const lexend = Lexend({ 
   subsets: ['latin'],
@@ -19,16 +19,19 @@ const lexend = Lexend({
 export const metadata = {
   title: 'Interlace Studies',
   description: 'Your Gateway to Global Education and Migration',
-  google: '_yvoOG6WHXKq8t7yi',
+  google: '_yvoOG6WHXKq8t7yi', // pastikan ini sesuai kode verifikasi dari Google
 }
-
-
-
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" dir="LTR">
-      <body  className={`${inter.variable} ${lexend.variable} font-inter text-base text-slate-950 dark:text-white dark:bg-slate-900`}>
+    <html lang="en" dir="ltr">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="_yvoOG6WHXKq8t7yivEfq89Yoh_q7BdebyWrUSGqZV8"
+        />
+      </head>
+      <body className={`${inter.variable} ${lexend.variable} font-inter text-base text-slate-950 dark:text-white dark:bg-slate-900`}>
         {children}
         <Analytics />
         <SpeedInsights />
