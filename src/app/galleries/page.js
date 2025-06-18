ed7af73fc04fe741babbc75f41a1f6ab7cc6b4af
Dewakayika <kayikadewa@gmail.com>
2025-06-18 12:22:49 +0800

    Update: Set Contentful environment to 'master' in gallery data fetching configuration

--- src/app/galleries/page.js
+++ src/app/galleries/page.js
@@ -6,6 +6,7 @@ import { createClient } from 'contentful';
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+  environment: 'master',
 });
 
 async function getGalleries() {
